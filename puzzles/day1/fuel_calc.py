943e0b2973acd118979de750d83a82a0af223344
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-02 17:29:34 +0000

    Add solution for Puzzle 2: improved fuel calculator

--- puzzles/day1/fuel_calc.py
+++ puzzles/day1/fuel_calc.py
@@ -15,9 +15,26 @@ def sum_fuel(fuel_list):
     return functools.reduce(lambda a, b: a+b, fuel_list)
 
 
+def calc_total_fuel(mass):
+    """
+    Return final fuel for mass, including fuel required for the fuel mass itself
+    """
+    fuel = fuel_for_mass(mass)
+
+    if fuel < 0:
+        return 0
+
+    added_fuel = calc_total_fuel(fuel)
+    return fuel + added_fuel
+
+
 if __name__ == '__main__':
     file_contents = open('input/mass.txt', 'r').read().splitlines()
     fuel_list = [fuel_for_mass(int(value)) for value in file_contents]
     result = sum_fuel(fuel_list)
 
+    total_fuel_list = [calc_total_fuel(int(value)) for value in file_contents]
+    improved_result = sum_fuel(total_fuel_list)
+
     print(result)
+    print(improved_result)
--- tests/fuel_calc_tests.py
+++ tests/fuel_calc_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from puzzles.day1.fuel_calc import fuel_for_mass, sum_fuel
+from puzzles.day1.fuel_calc import fuel_for_mass, sum_fuel, calc_total_fuel
 
 
 class TestFuelCalc(unittest.TestCase):
@@ -17,3 +17,9 @@ class TestFuelCalc(unittest.TestCase):
         self.assertEqual(sum_fuel([1, 2, 3]), 6)
         self.assertEqual(sum_fuel([12, 1, 24, 9]), 46)
         self.assertEqual(sum_fuel([3, 8, 2, 95]), 108)
+
+    def test_calc_total_fuel(self):
+        self.assertEqual(calc_total_fuel(-10), 0)
+        self.assertEqual(calc_total_fuel(14), 2)
+        self.assertEqual(calc_total_fuel(1969), 966)
+        self.assertEqual(calc_total_fuel(100756), 50346)
