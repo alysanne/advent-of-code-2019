7924903e6d10ad1fb6842d71bc52d2557d462446
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-09 22:53:33 +0000

    Add solution for Puzzle 4: gravity assist inputs

--- puzzles/day2/intcode.py
+++ puzzles/day2/intcode.py
@@ -23,6 +23,31 @@ def process_gravity_assist(values):
     return values
 
 
+def find_initial_inputs(instructions, expected_output):
+    print('Finding values...')
+
+    for i in range(0, 100):
+        for j in range(0, 100):
+            list_instr = _create_list(instructions)
+            values = _init_memory(i, j, list_instr)
+            result = process_gravity_assist(values)
+
+            if result[0] == expected_output:
+                return 'Found!: noun {}, verb {}'.format(i, j)
+
+    raise Exception('Initial inputs not found :(')
+
+
+def _init_memory(noun, verb, list_instr):
+    list_instr[1] = noun
+    list_instr[2] = verb
+    return list_instr
+
+
+def _create_list(values):
+    return list(map(lambda a: int(a), values.split(',')))
+
+
 if __name__ == '__main__':
     file_contents = open('input/gravity_assist.txt', 'r').read()
     values = list(map(lambda a: int(a), file_contents.split(",")))
@@ -32,5 +57,8 @@ if __name__ == '__main__':
     values[2] = 2
 
     result = process_gravity_assist(values)
+    print('Output: {}'.format(result[0]))
 
-    print(result)
+    # Find inputs for output 19690720
+    inputs = find_initial_inputs(file_contents, 19690720)
+    print(inputs)
--- tests/intcode_tests.py
+++ tests/intcode_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from puzzles.day2.intcode import process_gravity_assist
+from puzzles.day2.intcode import process_gravity_assist, find_initial_inputs
 
 
 class TestIntcode(unittest.TestCase):
@@ -10,3 +10,8 @@ class TestIntcode(unittest.TestCase):
         self.assertEqual(process_gravity_assist([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99])
         self.assertEqual(process_gravity_assist([2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801])
         self.assertEqual(process_gravity_assist([1, 1, 1, 4, 99, 5, 6, 0, 99]), [30, 1, 1, 4, 2, 5, 6, 0, 99])
+
+    def test_init_memory(self):
+        self.assertEqual(find_initial_inputs('1,0,0,3,1,3,1,0,99,30,40,50', 2), 'Found!: noun 0, verb 0')
+        self.assertEqual(find_initial_inputs('2,0,0,3,1,3,1,0,99,30,40,50', 4), 'Found!: noun 0, verb 0')
+        self.assertEqual(find_initial_inputs('1,0,0,3,2,3,3,0,99,30,40,50', 9), 'Found!: noun 0, verb 2')
