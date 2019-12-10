d9fff2a00a36d699ddf594a1820f900cb97577bd
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-10 22:31:38 +0000

    Add solution for Puzzle 6: crossed wires - min steps

--- puzzles/day3/crossed_wires.py
+++ puzzles/day3/crossed_wires.py
@@ -1,6 +1,6 @@
 def find_closest_intersection(cable1, cable2):
-    cable1_coordinates = get_coordinates_set(cable1)
-    cable2_coordinates = get_coordinates_set(cable2)
+    cable1_coordinates, _ = get_coordinates(cable1)
+    cable2_coordinates, _ = get_coordinates(cable2)
 
     matches = cable1_coordinates.intersection(cable2_coordinates)
 
@@ -11,14 +11,18 @@ def _calc_manhattan_dist(coordinates):
     return abs(coordinates[0]) + abs(coordinates[1])
 
 
-def get_coordinates_set(cable):
+def get_coordinates(cable):
     cable_coordinates = set()
+    steps_for_coordinates = {}
     cable_list = cable.split(',')
+    step_count = 0
 
     position = {'x': 0, 'y': 0}  # Cable position as (x, y)
 
     for move in cable_list:
         for i in range(int(move[1:])):
+            step_count += 1
+
             if move[0] == 'U':
                 position['y'] += 1
             elif move[0] == 'D':
@@ -28,13 +32,29 @@ def get_coordinates_set(cable):
             elif move[0] == 'L':
                 position['x'] -= 1
 
-            cable_coordinates.add((position['x'], position['y']))
+            coordinates = (position['x'], position['y'])
+            cable_coordinates.add(coordinates)
+            if coordinates not in steps_for_coordinates:
+                steps_for_coordinates[coordinates] = step_count
 
-    return cable_coordinates
+    return cable_coordinates, steps_for_coordinates
+
+
+def find_min_intersection(cable1, cable2):
+    cable1_coordinates, cable1_steps = get_coordinates(cable1)
+    cable2_coordinates, cable2_steps = get_coordinates(cable2)
+
+    matches = cable1_coordinates.intersection(cable2_coordinates)
+
+    return min([cable1_steps[match] + cable2_steps[match] for match in matches])
 
 
 if __name__ == '__main__':
     file_contents = open('input/cables.txt', 'r').readlines()
     result = find_closest_intersection(*file_contents)
 
-    print(result)
+    print('Closest intersection distance: {}'.format(result))
+
+    min_steps = find_min_intersection(*file_contents)
+
+    print('Min intersection steps: {}'.format(min_steps))
--- tests/crossed_wires_test.py
+++ tests/crossed_wires_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day3.crossed_wires import find_closest_intersection, get_coordinates_set
+from day3.crossed_wires import find_closest_intersection, get_coordinates, find_min_intersection
 
 
 class TestCrossedWires(unittest.TestCase):
@@ -11,4 +11,13 @@ class TestCrossedWires(unittest.TestCase):
                                                    'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 135)
 
     def test_get_coordinates_set(self):
-        self.assertEqual(get_coordinates_set('R1,D2,R1,U2'), {(1, 0), (1, -1), (1, -2), (2, -2), (2, -1), (2, 0)})
+        self.assertEqual(get_coordinates('R1,D2,R1,U2'), ({(1, 0), (1, -1), (1, -2), (2, -2), (2, -1), (2, 0)},
+                                                          {(1, 0): 1, (1, -1): 2, (1, -2): 3, (2, -2): 4,
+                                                           (2, -1): 5, (2, 0): 6}))
+        self.assertEqual(get_coordinates('R1,D1,U1'), ({(1, 0), (1, -1)}, {(1, 0): 1, (1, -1): 2}))
+
+    def test_find_min_intersection(self):
+        self.assertEqual(find_min_intersection('R75,D30,R83,U83,L12,D49,R71,U7,L72',
+                                               'U62,R66,U55,R34,D71,R55,D58,R83'), 610)
+        self.assertEqual(find_min_intersection('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+                                               'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 410)
