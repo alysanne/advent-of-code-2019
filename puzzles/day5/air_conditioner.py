ab9bf2a2fdcd91e3201b87ebcc721a1c7e9bdd76
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-13 13:02:56 +0000

    Add solution for Puzzle 10: Air Conditioner intcode program v2

--- puzzles/day5/air_conditioner.py
+++ puzzles/day5/air_conditioner.py
@@ -29,18 +29,70 @@ def process_intcode(values, input_programme):
             print(value)
             step = 2
 
+        # op_code 5: jump if non-zero
+        # op_code 6: jump if zero
+        if op_code in [5, 6]:
+            jump_to = handle_jump_ops(i, op_code, modes, values)
+            i = jump_to
+            continue
+
+        # op_code 7: less than
+        # op_code 8: equals
+        if op_code in [7, 8]:
+            handle_comparison_ops(i, op_code, modes, values)
+            step = 4
+
         # Skip to next instruction
         i += step
 
     return values
 
 
+def handle_jump_ops(i, op_code, modes, values):
+    parameters = {}
+    for position in range(1, 3):
+        parameters['parameter' + str(position)] = get_value_for_mode(i, position, modes, values)
+
+    if op_code == 5:
+        if parameters['parameter1'] != 0:
+            jump_to = parameters['parameter2']
+        else:
+            jump_to = i + 3
+
+    if op_code == 6:
+        if parameters['parameter1'] == 0:
+            jump_to = parameters['parameter2']
+        else:
+            jump_to = i + 3
+
+    return jump_to
+
+
+def handle_comparison_ops(i, op_code, modes, values):
+    parameters = {}
+    for position in range(1, 3):
+        parameters['parameter' + str(position)] = get_value_for_mode(i, position, modes, values)
+
+    if op_code == 7:
+        if parameters['parameter1'] < parameters['parameter2']:
+            values[values[i + 3]] = 1
+        else:
+            values[values[i + 3]] = 0
+
+    if op_code == 8:
+        if parameters['parameter1'] == parameters['parameter2']:
+            values[values[i + 3]] = 1
+        else:
+            values[values[i + 3]] = 0
+
+
 if __name__ == '__main__':
     file_contents = open('input/diagnostic.txt', 'r').read().split(',')
-    values = list(map(lambda x: int(x), file_contents))
 
     print("First run...")
+    values = list(map(lambda x: int(x), file_contents))
     process_intcode(values, 1)
 
     print("Second run...")
-    process_intcode(values, 5)
+    values2 = list(map(lambda x: int(x), file_contents))
+    process_intcode(values2, 5)
--- puzzles/day5/air_conditioner_tests.py
+++ puzzles/day5/air_conditioner_tests.py
@@ -21,3 +21,54 @@ class TestAirConditioner(unittest.TestCase):
     def test_process_intcode_io_mode(self):
         process_intcode([3, 1, 104, 2, 99], 1)
         self.assertEqual(sys.stdout.getvalue().strip(), '2')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_true_comparisons_position_mode(self):
+        process_intcode([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], 8)
+        self.assertEqual(sys.stdout.getvalue().strip(), '1')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_false_comparisons_position_mode(self):
+        process_intcode([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], 13)
+        self.assertEqual(sys.stdout.getvalue().strip(), '0')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_true_comparisons_immediate_mode(self):
+        process_intcode([3, 3, 1108, -1, 8, 3, 4, 3, 99], 8)
+        self.assertEqual(sys.stdout.getvalue().strip(), '1')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_false_comparisons_immediate_mode(self):
+        process_intcode([3, 3, 1107, -1, 8, 3, 4, 3, 99], 11)
+        self.assertEqual(sys.stdout.getvalue().strip(), '0')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_true_jump_position_mode(self):
+        process_intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], 0)
+        self.assertEqual(sys.stdout.getvalue().strip(), '0')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_false_jump_immediate_mode(self):
+        process_intcode([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], 3)
+        self.assertEqual(sys.stdout.getvalue().strip(), '1')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_long_input_1(self):
+        process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                         999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 3)
+        self.assertEqual(sys.stdout.getvalue().strip(), '999')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_long_input_2(self):
+        process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                         999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 8)
+        self.assertEqual(sys.stdout.getvalue().strip(), '1000')
+
+    @patch('sys.stdout', new=io.StringIO())
+    def test_process_intcode_long_input_3(self):
+        process_intcode([3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                         1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                         999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], 21)
+        self.assertEqual(sys.stdout.getvalue().strip(), '1001')
