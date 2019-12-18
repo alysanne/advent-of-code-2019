08915a8945f7030a1b845b2516c00c87e5e33992
Natalia Fernández <natalia.fpintos@gmail.com>
2019-12-18 09:10:26 +0000

    Add solution for Puzzle 12: Orbit distance

--- puzzles/day6/universal_orbit_map.py
+++ puzzles/day6/universal_orbit_map.py
@@ -8,6 +8,21 @@ def checksum_orbits(orbit_list):
     return result
 
 
+def calculate_distance(orbit_list):
+    orbit_map = _create_orbit_map(orbit_list)
+    you_path = _path_to('YOU', orbit_map)
+    santa_path = _path_to('SAN', orbit_map)
+
+    # Remove YOU and SAN from calculations
+    you_path.remove('YOU')
+    santa_path.remove('SAN')
+
+    you_diff = you_path.difference(santa_path)
+    santa_diff = santa_path.difference(you_path)
+
+    return len(you_diff) + len(santa_diff)
+
+
 def _create_orbit_map(orbit_list):
     orbits = {}
     for orbit in orbit_list:
@@ -22,8 +37,20 @@ def _get_orbits(obj, orbit_map):
     return 0
 
 
+def _path_to(obj, orbit_map):
+    if obj in orbit_map:
+        path_to = _path_to(orbit_map[obj], orbit_map)
+        path_to.add(obj)
+        return path_to
+    else:
+        return set()
+
+
 if __name__ == '__main__':
     file_contents = open('input/map_data.txt').read().splitlines()
 
     result = checksum_orbits(file_contents)
     print("Number of orbits: {}".format(result))
+
+    result2 = calculate_distance(file_contents)
+    print("Distance to santa: {}".format(result2))
--- tests/universal_orbit_map_tests.py
+++ tests/universal_orbit_map_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day6.universal_orbit_map import checksum_orbits
+from day6.universal_orbit_map import checksum_orbits, calculate_distance
 
 
 class TestUniversalOrbitMap(unittest.TestCase):
@@ -8,11 +8,6 @@ class TestUniversalOrbitMap(unittest.TestCase):
         self.assertEqual(checksum_orbits(['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']), 42)
         self.assertEqual(checksum_orbits(['E)F', 'B)C', 'B)G', 'J)K', 'K)L', 'G)H', 'COM)B', 'D)E', 'D)I', 'E)J', 'C)D']), 42)
 
-
-
-
-
-
-
-
-
+    def test_calculate_distance(self):
+        self.assertEqual(calculate_distance(['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN']), 4)
+        self.assertEqual(calculate_distance(['E)F', 'B)C', 'I)SAN', 'B)G', 'J)K', 'K)YOU', 'K)L', 'G)H', 'COM)B', 'D)E', 'D)I', 'E)J', 'C)D']), 4)
